ba96ce80eb2dd31b0e6bb5740490b23171507906
Mark Huigen <mhuigen1992@gmail.com>
2014-02-02 15:52:24 +0100

    Bewegen incl achtergrond

--- javascript.js
+++ javascript.js
@@ -1,54 +1,77 @@
-(function() 
+
+//--functie om de animatie aan te maken--//
+  (function() 
   {
     		var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
    			window.requestAnimationFrame = requestAnimationFrame;
-	})();
+	 })();
 
-		var canvas = document.getElementById("canvas"),
- 			ctx = canvas.getContext("2d"),
+    //--background canvas--//
+    var bgcanvas = document.getElementById("bgcanvas"),
+        bgctx = bgcanvas.getContext("2d"),
+        width = 800,
+        height = 600;
+
+    //--inladen achtergrond--//
+    var achtergrond = new Image();
+    achtergrond.src = "bg.png";    
+
+    //--avatar canvas--//
+		var avcanvas = document.getElementById("avcanvas"),
+        avctx = avcanvas.getContext("2d"),
   			width = 800,
    			height = 600,
    			player = {
    	 		x : width/2,
-   	   		y : height/2,
+   	   		y : height*(9/10),
   	    	width : 5,
   	   		height : 5,
-          speed: 3,
+          speed: 1,
           velX: 0,
-          velY: 0
+          velY: 0,
+          draw: function() {
+            
+            canvas.fillRect(this.x, this.y, this.width, this.height);
+          }
   	 		},
         keys = [],
         friction = 0.8;
+
+    //--geef canvas correcte grootte mee--//
+		avcanvas.width = width;
+		avcanvas.height = height;
+    bgcanvas.width = width;
+    bgcanvas.height = height;
+
  
-		canvas.width = width;
-		canvas.height = height;
- 
+    //--speler laten bewegen dmv pijltjestoetsen--//
     function update(){
 
-      // check keys
+      //--pijltjestoetsen toewijzen--//
+
         if (keys[38]) {
-        // up arrow
-        if (player.velY < player.speed) {                         
+        //--pijltje naar boven--//
+        if (player.velY > -player.speed) {                         
           player.velY--;                  
           } 
         }
 
         if (keys[40]) {
-        // down arrow
+        //--pijltje naar beneden--//
         if (player.velY < player.speed) {                         
           player.velY++;                  
           } 
         }
 
         if (keys[39]) {
-          // right arrow
+          //--pijltje naar rechts--//
           if (player.velX < player.speed) {                         
           player.velX++;                  
           }          
         } 
 
         if (keys[37]) {                 
-          // left arrow                  
+          //--pijltje naar links--//                 
           if (player.velX > -player.speed) {
           player.velX--;
           }
@@ -72,27 +95,43 @@
       {
         player.y = height-player.height;
       } 
-      else if (player.y <= 0) 
+      else if (player.y <= height/2 + height/10) 
       {
-        player.y = 0;
+        player.y = height/2 + height/10;
       }
 
-      // draw our player
-      ctx.clearRect(0,0,width,height);
-      ctx.fillStyle = "red";
-      ctx.fillRect(player.x, player.y, player.width, player.height);
-      // run through the loop again
+      //--teken rood vierkant--//
+      avctx.clearRect(0,0,width,height);
+      avctx.fillStyle = "red";
+      avctx.fillRect(player.x, player.y, player.width, player.height);
+      //--update tekenen--//
       requestAnimationFrame(update);
     }
 
-document.body.addEventListener("keydown", function(e) {
-    keys[e.keyCode] = true;
-});
- 
-document.body.addEventListener("keyup", function(e) {
-    keys[e.keyCode] = false;
-});
+    //--achtergrond in laten laden ingame--//
+    function loadBg(){
+      clearBg();
+      bgctx.drawImage(achtergrond,0,0,800,600,0,0,800,600);
+      console.log("YOLOLO");
+    }
 
-window.addEventListener("load", function(){
-  update();
-});
+    //--achtergrond leegmaken--//
+    function clearBg(){
+      bgctx.clearRect(0,0,800,600);
+      console.log("pleaserespond");
+    }
+
+  //--eventlistener koppelen aan toetsen--//
+  document.body.addEventListener("keydown", function(e) {
+    keys[e.keyCode] = true;
+  });
+ 
+  document.body.addEventListener("keyup", function(e) {
+    keys[e.keyCode] = false;
+  });
+
+  //--eventlistener koppelen aan het in te laden scherm--//
+  window.addEventListener("load", function(){
+    update();
+    loadBg();
+  });
