453cdcf76f03eec10d97e2c46dd75d5bdd0d6aee
Mark Huigen <mhuigen1992@gmail.com>
2014-02-02 15:50:15 +0100

    Inladen achtergrond

---
+++ scriptfile.js
@@ -0,0 +1,30 @@
+
+
+// canvas eigenschappen
+var cvbg = document.getElementById('cvbg')
+var ctxbg.getContext('2d');
+
+var cvplayer = document.getElementById('cvplayer')
+var ctxplayer.getContext('2d');
+
+// declaratie prepositories
+
+var avatarrepo = new function{
+
+	// declaratie plaatjes
+	this.goodavatar = new Image();
+	this.mediumavatar = new Image();
+	this.badavatar = new Image();
+
+	// locatie plaatjes
+	this.goodavatar.src = "images/goodavatar.png";
+	this.mediumavatar.src = "images/mediumavatar.png";
+	this.badavatar.src = "images/badavatar.png";
+
+}
+
+
+
+
+
+
